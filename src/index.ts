99b327971480cd1809441dbdfefdcaa00c685ab3
Joel Field <joelmatthewfield@outlook.com>
2019-12-30 14:31:53 +1000

    allow creating an item

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import ItemsRouter from "./routes/item";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.SERVER_PORT; // default port to listen
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -19,9 +21,9 @@ db.once("open", () => console.log("connected to database"));
 app.use("/item", ItemsRouter);
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
-  res.send("Hello ");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello ");
+// });
 
 // start the Express server
 app.listen(port, () => {
--- src/routes/item/index.ts
+++ src/routes/item/index.ts
@@ -1,9 +1,28 @@
 import { Request, Response, Router } from "express";
 import Item from "../../models/item";
 
-const routes = Router().get("/", async (req: Request, res: Response) => {
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
   const items = await Item.find({});
   return res.json(items);
 });
 
-export default routes;
+router.post("/", async (req: Request, res: Response) => {
+  const { name, price, url, category } = req.body;
+  const item = new Item({
+    name,
+    price,
+    url,
+    category
+  });
+
+  try {
+    const newItem = await item.save();
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+export default router;
