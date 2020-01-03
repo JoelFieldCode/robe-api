0db2ff3458c81241aa9dfecbd76c3923c80086a6
Joel Field <joelmatthewfield@outlook.com>
2020-01-03 15:13:52 +1000

    add get single item API

--- src/routes/item/index.ts
+++ src/routes/item/index.ts
@@ -1,11 +1,27 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router, NextFunction } from "express";
 import Item from "../../models/item";
-
+import HttpException from "../../exceptions/HttpException";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const items = await Item.find({ user_id: req.context.user_id });
   return res.json(items);
+});
+
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    const itemUserId = item.toJSON().user_id;
+    if (!item || itemUserId !== req.context.user_id) {
+      return next(new HttpException(404));
+    }
+    return res.json(item);
+  } catch (err) {
+    if (err.name === "CastError") {
+      return next(new HttpException(400));
+    }
+    return next(new HttpException(404));
+  }
 });
 
 router.post("/", async (req: Request, res: Response) => {
