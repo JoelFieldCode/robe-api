8ea65971ed6c634665da67e8939d43aea2454759
Joel Field <joelmatthewfield@outlook.com>
2020-05-02 14:40:38 +1000

    let error middleware catch error

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,13 +3,16 @@ import { login } from "../../services/auth/login";
 
 const router = Router();
 
-router.post("/login", async (req: Request, res: Response) => {
-  try {
-    await login(req);
-    return res.json("Success").status(200);
-  } catch (e) {
-    throw e;
+router.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await login(req);
+      return res.json("Success").status(200);
+    } catch (e) {
+      next(e);
+    }
   }
-});
+);
 
 export default router;
