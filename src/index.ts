cceac9c9de84f1f7c6692a15dd4f663c0e5d8b4c
Joel Field <joelmatthewfield@outlook.com>
2022-07-09 21:03:55 +1000

    graphql example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { json, NextFunction, Request, Response } from "express";
+import express, { json, NextFunction, Request, Response, Express } from "express";
 import bearerToken from "express-bearer-token";
 import HttpException from "./exceptions/HttpException";
 import errorMiddleware from "./middleware/errorMiddleware";
 import AuthRouter from "./routes/auth";
 import CategoryRouter from "./routes/category";
 import ItemsRouter from "./routes/item";
+import { graphqlHTTP } from 'express-graphql'
+import { schema, resolver } from './schema'
 
 dotenv.config();
 
@@ -26,7 +28,7 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(bearerToken());
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req, _res, next) => {
   req.context = {};
   next();
 });
@@ -36,9 +38,15 @@ const PORT = process.env.PORT || 8080;
 app.use("/item", ItemsRouter);
 app.use("/category", CategoryRouter);
 app.use("/auth", AuthRouter);
+// todo fix type
 app.use((err: HttpException, req: Request, res: Response, next: NextFunction) =>
   errorMiddleware(err, req, res, next)
 );
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: resolver,
+  graphiql: true,
+}));
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
