f9729bea29661ede3215aee8e2f8d6e7165523b2
Joel Field <joelmatthewfield@outlook.com>
2023-03-18 21:39:59 +1000

    create category mutation

--- src/schema/resolver.ts
+++ src/schema/resolver.ts
@@ -5,8 +5,15 @@ const prisma = new PrismaClient()
 
 type GetCategoryQuery = { categoryId: number }
 
+type CreateCategoryMutation = {
+  input: {
+    name: string,
+    image_url: string
+  }
+}
+
 export const resolver = {
-  getCategories: async (args: {}, req: Request) => {
+  getCategories: async (_args: {}, req: Request) => {
     return await prisma.category.findMany({
       where: {
         user_id: req.context.user_id,
@@ -14,11 +21,23 @@ export const resolver = {
     })
   },
   getCategory: async ({ categoryId }: GetCategoryQuery, req: Request) => {
-    return await prisma.category.findFirst({
+    return await prisma.category.findFirstOrThrow({
       where: {
         id: categoryId,
         user_id: req.context.user_id,
       }
     })
+  },
+  getCategoryItems: async ({ categoryId }: GetCategoryQuery, req: Request) => {
+    return await prisma.item.findMany({
+      where: {
+        categoryId,
+        user_id: req.context.user_id,
+      }
+    })
+  },
+  createCategory: async ({ input }: CreateCategoryMutation, req: Request) => {
+    const { name, image_url } = input
+    return await prisma.category.create({ data: { name, image_url, user_id: req.context.user_id } })
   }
 };
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -2,13 +2,28 @@ import { buildSchema } from 'graphql'
 
 // Construct a schema, using GraphQL schema language
 export const schema = buildSchema(`
+    input CreateCategoryInput {
+        name: String!
+        image_url: String!
+    }
   type Category {
     name: String!
     id: Int!
     image_url: String!
   }
+  type Item {
+    name: String!
+    id: Int!
+    image_url: String!
+    url: String!
+    price: Float!
+  }
   type Query {
     getCategories: [Category!]
     getCategory(categoryId: Int!): Category
+    getCategoryItems(categoryId: Int!): [Item!]
+  }
+  type Mutation {
+    createCategory(input: CreateCategoryInput): Category
   }
 `)
