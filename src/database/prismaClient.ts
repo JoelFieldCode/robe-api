f1e603cd277d541a1181fe173cc3c8f0fba400ec
Joel Field <joelmatthewfield@outlook.com>
2025-02-15 19:51:15 +1000

    upgrade prisma and use neon

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,5 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import ws from 'ws'
 
-const prisma = new PrismaClient();
+declare global {
+   var prisma: PrismaClient | undefined
+}
 
-export { prisma };
+dotenv.config()
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+const prisma = global.prisma || new PrismaClient({ adapter });
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export {prisma}
